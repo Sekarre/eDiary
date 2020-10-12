739d0ea82a206a9844f603ac0a564f46917d7725
Sekarre <zimny2121@gmail.com>
2020-10-12 20:05:03 +0200

    Initial commit

---
+++ src/main/java/com/ediary/EdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.ediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdiaryApplication.class, args);
+    }
+
+}
