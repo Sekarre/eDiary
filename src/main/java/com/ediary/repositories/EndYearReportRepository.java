27b3350ece31d9b75d406ce963532408714e34a0
RaggerLtp <ragger.break@gmail.com>
2020-12-11 14:11:53 +0100

    zmiana zapytania w repozytorium EndYearReport

--- src/main/java/com/ediary/repositories/EndYearReportRepository.java
+++ src/main/java/com/ediary/repositories/EndYearReportRepository.java
@@ -1,6 +1,5 @@
 package com.ediary.repositories;
 
-import com.ediary.domain.Class;
 import com.ediary.domain.EndYearReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +7,6 @@ import java.util.List;
 
 public interface EndYearReportRepository  extends JpaRepository<EndYearReport, Long> {
 
-    List<EndYearReport> findAllByUserType_Student();
-    List<EndYearReport> findAllByUserType_Teacher();
+    List<EndYearReport> findAllByUserTypeOrderByYearAsc(EndYearReport.Type type);
 
 }
